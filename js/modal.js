19aa75f04d32a5d1de4bb7b1cedb2369d960d04e
Heorhii Honcharov <0019@ukr.net>
2023-01-06 14:28:35 +0100

    modified:   CSS/styles.css 	modified:   images/symbol-defs.svg 	modified:   index.html 	modified:   js/modal.js

--- js/modal.js
+++ js/modal.js
@@ -4,11 +4,23 @@
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
+  
+  const toggleModal = () => {
+    const isModalOpen =
+      refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
+    refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
+    refs.modal.classList.toggle('is-hidden');
+
+    const scrollLockMethod = !isModalOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
+  // function toggleModal() {
+  //   refs.modal.classList.toggle("is-hidden");
+  // }
 })();
