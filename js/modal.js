49c15f8de9e53eab01bcf68a22a31afca742a232
Heorhii Honcharov <0019@ukr.net>
2023-01-09 14:23:14 +0100

    Rebilding

--- js/modal.js
+++ js/modal.js
@@ -20,7 +20,4 @@
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  // function toggleModal() {
-  //   refs.modal.classList.toggle("is-hidden");
-  // }
 })();
